9c090691284720d2ff65064817e276c5e60cf5c2
scorp <3048453@gmail.com>
2023-12-04 21:34:40 +0300

    Initial commit

---
+++ src/main/java/ru/alex/bank_managersystem/BankManagerSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.alex.bank_managersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankManagerSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alex/bank_managersystem/BankManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alex.bank_managersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
