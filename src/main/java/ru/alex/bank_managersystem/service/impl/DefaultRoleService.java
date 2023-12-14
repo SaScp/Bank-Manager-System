dac8409d185c28048edad50b74efb3725440a4df
scorp <3048453@gmail.com>
2023-12-14 22:16:20 +0300

    delete RoleService.java

--- src/main/java/ru/alex/bank_managersystem/service/impl/DefaultRoleService.java
+++
@@ -1,8 +0,0 @@
-package ru.alex.bank_managersystem.service.impl;
-
-import org.springframework.stereotype.Service;
-import ru.alex.bank_managersystem.service.AccountService;
-import ru.alex.bank_managersystem.service.RoleService;
-@Service
-public class DefaultRoleService implements RoleService {
-}
